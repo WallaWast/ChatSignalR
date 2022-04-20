defae659cee28659ee20f23a60eb8aecf671a61a
Wallace Torres <wallacewst@gmail.com>
2022-04-20 10:40:54 -0700

    Scrroling on new messages and getting user name on conection

--- ChatSignalR/Server/Hubs/ChatHub.cs
+++ ChatSignalR/Server/Hubs/ChatHub.cs
@@ -4,10 +4,21 @@ namespace ChatSignalR.Server.Hubs
 {
     public class ChatHub : Hub
     {
+        private static Dictionary<string, string> Users = new();
+
         public override async Task OnConnectedAsync()
         {
-            await AddMessageToChat("", "User connected!");
+            string username = Context.GetHttpContext().Request.Query["username"];
+            Users.Add(Context.ConnectionId, username);
+            await AddMessageToChat(string.Empty, $"{username} joined!");
             await base.OnConnectedAsync();
+        }
+
+        public override async Task OnDisconnectedAsync(Exception ex)
+        {
+            string userName = Users[Context.ConnectionId];
+            Users.Remove(Context.ConnectionId);
+            await AddMessageToChat(string.Empty, $"{userName} left!");
         }
 
         public async Task AddMessageToChat(string user, string message)
